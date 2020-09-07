1bfcdf62553febb0779da139a29a9634e0ceffa7
Ahmed Ehab <68254594+ahmedehab52@users.noreply.github.com>
2020-09-07 16:21:20 +0200

    Add files via upload

---
+++ OBC/MCAL/ADC.h
@@ -0,0 +1,23 @@
+/*
+ * ADC.h
+ *
+ * Created: 1/26/2020 11:41:38 PM
+ *  Author: Ahmed
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <avr/io.h>
+
+
+void ADC0_init(void);
+uint8_t ADC_Read(void);
+
+
+
+
+
+
+#endif /* ADC_H_ */
